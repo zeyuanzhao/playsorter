5a0ce8f7469a3e64eefa7e7d62d69bad542ea7dc
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-03-04 08:46:43 -0500

    fix auth navbar

--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -1,10 +1,19 @@
+"use client";
+
+import { UserContext } from "@/app/providers";
 import Navbar from "@/components/Navbar";
+import { useContext } from "react";
 
 const Template = ({ children }: { children: React.ReactNode }) => {
+  const userContext = useContext(UserContext);
+  if (!userContext) {
+    throw new Error("UserContext must be used within a UserProvider");
+  }
+  const [user] = userContext;
   return (
     <div className="px-6">
       <div>
-        <Navbar />
+        <Navbar user={user} />
       </div>
       {children}
     </div>
