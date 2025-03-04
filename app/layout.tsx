293d9ff2299a51131ec93918cb7d4396507524fa
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-03-04 09:18:11 -0500

    remove unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
-import LoadingScreen from "@/components/LoadingScreen";
 
 export const metadata: Metadata = {
   title: "SpotiList",
